14760e6f21cfd082701ec909371145a14f478cca
Diego Lima <ddddiegolima@gmail.com>
2018-02-15 10:27:11 -0300

    settings com DB e sample de local settings

--- atena/atena/settings.py
+++ atena/atena/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'atena.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'atena',
-        'USER': 'postgres',
-        'PASSWORD': 'pgadmin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'atena',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pgadmin',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
