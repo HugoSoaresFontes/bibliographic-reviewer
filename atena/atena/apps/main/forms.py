17512281914882c482db20a7a82927c52ab24569
Hugo <hugogsoaresfontes@gmail.com>
2018-02-26 18:43:23 -0300

    M.P

--- atena/atena/apps/main/forms.py
+++ atena/atena/apps/main/forms.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django import forms
 
+from .helpers import RevisaoHelper, FichamentoHelper
 from .models import Revisao, Documento, Fichamento
-from .helpers import RevisaoHelper
 from base.forms import BaseForm
 
 
@@ -12,7 +12,7 @@ class RevisaoForm(BaseForm):
 
     class Meta:
         model = Revisao
-        exclude = ['adm']
+        exclude = ['adm', 'documentos']
 
     def __init__(self, *args, **kwargs):
         super(RevisaoForm, self).__init__(*args, **kwargs)
@@ -27,3 +27,27 @@ class RevisaoForm(BaseForm):
 
 
         return revisao
+
+
+class FichamentoForm(BaseForm):
+
+    class Meta:
+        model = Fichamento
+        exclude = ['documento', 'revisao']
+
+    def __init__(self, *args, **kwargs):
+        self.revisao = kwargs.pop('revisao')
+        self.documento = kwargs.pop('documento')
+        super(FichamentoForm, self).__init__(*args, **kwargs)
+        self.helper = FichamentoHelper()
+
+    def save(self, commit=True):
+        fichamento = super(FichamentoForm, self).save(commit=False)
+        fichamento.documento = self.documento
+        fichamento.revisao = self.revisao
+        
+        if commit:
+            fichamento.save()
+
+
+        return fichamento
